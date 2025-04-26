305915e7e4cb12ed6b599177bd4b8fd62abcf215
Sami Ali <samialitop3@gmail.com>
2025-04-26 00:25:26 -0400

    [TASK-CC-1900] Low-code agents - Creating libraries to reduce agent construction to a few lines of code

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cc-agent-foundry",
+    version="0.1.0",
+    description="Shared Agent Template Library Code",
+    author="CloudCanaries",
+    author_email="support@cloudcanaries.ai",
+    url="https://github.com/CloudCanaries/cc-agent-foundry",
+    python_requires=">=3.8",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
